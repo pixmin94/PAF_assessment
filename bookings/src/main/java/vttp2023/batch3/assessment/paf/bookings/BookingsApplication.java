af432c2cb0215631391ad72e1727dc6ba05af18e
pixmin94 <pixmin@outlook.com>
2023-07-21 12:00:13 +0800

    Task3 mongo qry and java method to get search list

--- bookings/src/main/java/vttp2023/batch3/assessment/paf/bookings/BookingsApplication.java
+++ bookings/src/main/java/vttp2023/batch3/assessment/paf/bookings/BookingsApplication.java
@@ -2,30 +2,39 @@ package vttp2023.batch3.assessment.paf.bookings;
 
 import java.util.List;
 
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import vttp2023.batch3.assessment.paf.bookings.models.Search;
 import vttp2023.batch3.assessment.paf.bookings.repositories.ListingsRepository;
 
 @SpringBootApplication
-public class BookingsApplication { //implements CommandLineRunner {
+public class BookingsApplication implements CommandLineRunner {
 
-	// @Autowired
-	// private ListingsRepository repo;
+	@Autowired
+	private ListingsRepository repo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookingsApplication.class, args);
 	}
 
-	// @Override
-	// public void run(String... args) throws Exception {
-	// 	List<String> result = repo.getAllCountries();
-	// 	System.out.println(">>>>>>>>>>> \n");
-	// 	for (String c : result) {
-	// 		System.out.println(c);
-	// 	}
-	// }
+	@Override
+	public void run(String... args) throws Exception {
+		// List<String> result = repo.getAllCountries();
+		Search s = new Search();
+		s.setCountry("Australia");
+		s.setPax(2);
+		s.setMin(1);
+		s.setMax(100);
+		List<Document> result = repo.getSearchListings(s);
+		System.out.println(">>>>>>>>>>> \n");
+		for (Document c : result) {
+			System.out.println(c);
+		}
+		System.exit(0);
+	}
 
 }
--- bookings/src/main/java/vttp2023/batch3/assessment/paf/bookings/repositories/ListingsRepository.java
+++ bookings/src/main/java/vttp2023/batch3/assessment/paf/bookings/repositories/ListingsRepository.java
@@ -2,11 +2,22 @@ package vttp2023.batch3.assessment.paf.bookings.repositories;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.stereotype.Repository;
+import org.bson.Document;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.aggregation.MatchOperation;
+import org.springframework.data.mongodb.core.aggregation.ProjectionOperation;
+import org.springframework.data.mongodb.core.aggregation.SortOperation;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+
+import vttp2023.batch3.assessment.paf.bookings.models.Search;
 
 @Repository
 public class ListingsRepository {
@@ -23,7 +34,37 @@ public class ListingsRepository {
 		return countries;
 	}
 	
-	//TODO: Task 3
+	/* db.listings.aggregate([
+		{ $match: {
+			$and: [
+				{ "address.country" : "Australia"}, 
+				{ "accommodates" : {$gte: 2} },
+				{ "price" : {$gte: 1, $lte: 100} }
+			]
+		} },
+		{ $sort: {price: -1} },
+		{ $project: {_id: 0, name: 1, "images.picture_url": 1, price: 1}}
+	]) */
+	public List<Document> getSearchListings(Search search) {
+		Criteria c = new Criteria().andOperator(
+			Criteria.where("address.country").is(search.getCountry()),
+			Criteria.where("accommodates").gte(search.getPax()),
+			Criteria.where("price").gte(search.getMin()).lte(search.getMax())
+			);
+		MatchOperation match = Aggregation.match(c);
+
+		SortOperation sort = Aggregation.sort(Sort.by(Direction.DESC, "price"));
+
+		ProjectionOperation project = Aggregation.project(
+			"name", "images.picture_url", "price")
+			.andExclude("_id");
+
+		Aggregation pipeline = Aggregation.newAggregation(match, sort, project);
+
+		AggregationResults<Document> result = template.aggregate(pipeline, C_LISTINGS, Document.class);
+		
+		return result.getMappedResults();
+	}
 
 
 	//TODO: Task 4
